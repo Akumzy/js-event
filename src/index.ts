3cb0b530a8fb079488e6e42ac2fd49eaa000b354
Akuma Isaac Akuma <akumaisaacakuma@gmail.com>
2020-06-08 10:59:35 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 export default class JSEvent {
-  #listener: Map<string, Function[]> = new Map()
-  
+  #listeners: Map<string, Function[]> = new Map()
   dispatchEvent = (event: string, payload: any) => {
-    let handlers = this.#listener.get(event)
+    let handlers = this.#listeners.get(event)
     if (Array.isArray(handlers)) {
       handlers.forEach((h) => h(payload))
     }
     return this
   }
-  addEventListener = (event: string, cb: Function) => {
-    let handlers = this.#listener.get(event)
+  addEventListeners = (event: string, cb: Function) => {
+    let handlers = this.#listeners.get(event)
     handlers = Array.isArray(handlers) ? [...handlers, cb] : [cb]
-    this.#listener.set(event, handlers)
+    this.#listeners.set(event, handlers)
     return this
   }
-  removeEventListener = (event: string, cb: Function) => {
-    let handlers = this.#listener.get(event)
+  removeEventListeners = (event: string, cb: Function) => {
+    let handlers = this.#listeners.get(event)
     if (Array.isArray(handlers)) {
       handlers = handlers.filter((h) => h !== cb)
-      this.#listener.set(event, handlers)
+      this.#listeners.set(event, handlers)
     }
     return this
   }
